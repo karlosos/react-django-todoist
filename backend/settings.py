dd6007db0b7d3ee262737da4d75ca3eab53a3955
Karol Dzialowski <karlososhd@gmail.com>
2020-07-01 22:56:36 +0200

    feat: get tasks from backend

--- backend/settings.py
+++ backend/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'backend.todo',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,3 +130,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,50 +1,28 @@
 import { useState, useEffect } from 'react'
-import moment from 'moment'
 import { firebase } from '../firebase'
-import { collatedTasksExist } from '../helpers'
+import axios from 'axios'
 
 export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([])
   const [archivedTasks, setArchivedTasks] = useState([])
 
   useEffect(() => {
-    let unsubscribe = firebase
-      .firestore()
-      .collection('tasks')
-      .where('userId', '==', 'jlIFXIwyAL3tzHMtzRbw')
+    const requestParams = selectedProject && selectedProject !== 'INBOX'
+      ? { filter: selectedProject } : {}
 
-    unsubscribe =
-      selectedProject && !collatedTasksExist(selectedProject)
-        ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
-        : selectedProject === 'TODAY'
-          ? (unsubscribe = unsubscribe.where(
-            'date',
-            '==',
-            moment().format('DD/MM/YYYY')
-          ))
-          : selectedProject === 'INBOX' || selectedProject === 0
-            ? (unsubscribe = unsubscribe.where('date', '==', ''))
-            : unsubscribe
-
-    unsubscribe = unsubscribe.onSnapshot(snapshot => {
-      const newTasks = snapshot.docs.map(task => ({
-        id: task.id,
-        ...task.data()
-      }))
-
-      setTasks(
-        selectedProject === 'NEXT_7'
-          ? newTasks.filter(
-            task =>
-              moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
-                task.archived !== true
-          )
-          : newTasks.filter(task => task.archived !== true)
-      )
-      setArchivedTasks(newTasks.filter(task => task.archived !== false))
+    axios.get('http://127.0.0.1:8000/api/v1/tasks/', {
+      params: requestParams
     })
-
-    return () => unsubscribe()
+      .then(res => {
+        const tasks = res.data
+        setTasks(tasks.filter(task => task.archived === false))
+        setArchivedTasks(tasks.filter(task => task.archived === true))
+      })
+      .catch(err => {
+        setTasks([])
+        setArchivedTasks([])
+        console.log(err)
+      })
   }, [selectedProject])
 
   return { tasks, archivedTasks }
