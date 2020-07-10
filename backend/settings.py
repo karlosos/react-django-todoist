162d28be113b6d9911442f79246e91fdd57ef53b
Karol Dzialowski <karlososhd@gmail.com>
2020-07-10 14:32:27 +0200

    feat: heroku deploy

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
 # ]
 
 # Configure app for Heroku deployment
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
